88acd7ef5bb8f7655519f093ada2e7caff5df4e8
shilpa2ks <shilpa2ks@gmail.com>
2026-01-20 19:05:12 +0800

    updated import

--- explore-planets/planets/components/planetary-stats/PlanetaryStats.tsx
+++ explore-planets/planets/components/planetary-stats/PlanetaryStats.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { PlanetaryStatsProps } from "../types";
 import React from "react";
 
